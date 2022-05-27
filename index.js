b3aa9299ddc0c6ffd7e3029f46e604e46e701f22
Princewill <princesolo919@gmail.com>
2022-05-27 22:00:41 +0100

    added backend server

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
+const app = express()
+
+app.get('/',function (req,res) {
+    res.send('hello will')
+})
+
+app.listen(8000,function () {
+    console.log('started')
+})
