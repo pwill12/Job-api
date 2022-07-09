b7ece82c42ca1e9270433c1edcaa9722057ba0d2
unknown <princesolo919@gmail.com>
2022-07-09 05:13:16 +0100

    modified api

--- models/Users.js
+++ models/Users.js
@@ -1,28 +1,25 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema(
-  {
+const UserSchema = new mongoose.Schema({
     img: { type: String, required: false, unique: false },
     username: { type: String, required: true, unique: true },
     firstname: { type: String, required: false },
     lastname: { type: String, required: false, unique: false },
-    number: {type: Number, unique: true},
-    website: {type: String},
-    linked: {type: String},
-    github: {type: String},
-    facebook: {type: String},
-    instagram: {type: String},
-    twitter: {type: String},
-    skills: { type: Array},
+    number: { type: Number, required: false, unique: true },
+    website: { type: String },
+    linked: { type: String },
+    github: { type: String },
+    facebook: { type: String },
+    instagram: { type: String },
+    twitter: { type: String },
+    skills: { type: Array },
     location: { type: Array, required: false },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, unique: true },
+    password: { type: String, required: true, },
     isAdmin: {
-      type: Boolean,
-      default: false,
+        type: Boolean,
+        default: false,
     },
-  },
-  { timestamps: true }
-);
+}, { timestamps: true });
 
 module.exports = mongoose.model("User", UserSchema);
--- routes/Auth.js
+++ routes/Auth.js
@@ -5,73 +5,78 @@ const jwt = require("jsonwebtoken");
 
 //Register Route
 
-router.post("/register", async (req, res) => {
+router.post("/register", async(req, res) => {
 
-  const newUser = new User({
-    img: req.body.img,
-    username: req.body.username,
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-    skills: req.body.skills,
-    website: req.body.website,
-    number: req.body.number,
-    twitter: req.body.twitter,
-    facebook: req.body.facebook,
-    instagram: req.body.instagram,
-    linked: req.body.linked,
-    github: req.body.github,
-    email: req.body.email,
-    password: CryptoJS.AES.encrypt(
-      req.body.password,
-      process.env.PASS_SEC
-    ).toString(),
-  });
+    const newUser = new User({
+        img: req.body.img,
+        username: req.body.username,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        skills: req.body.skills,
+        website: req.body.website,
+        number: req.body.number,
+        twitter: req.body.twitter,
+        facebook: req.body.facebook,
+        instagram: req.body.instagram,
+        linked: req.body.linked,
+        github: req.body.github,
+        email: req.body.email,
+        password: CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString(),
+    });
 
-  try {
-    const savedUser = await newUser.save();
-    res.status(201).json(savedUser);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+    try {
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
-router.post("/login", async (req, res) => {
-  try {
-    const user = await User.findOne({ username: req.body.username });
+router.post("/login", async(req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
 
-    !user && res.status(401).json("Wrong User Name");
+        if (!user) {
+            res.status(401).json("Wrong User Name")
+        }
+        // else{
+        //   res.status(200).json("Wrong User Name")
+        // }
 
-    const hashedPassword = CryptoJS.AES.decrypt(
-      user.password,
-      process.env.PASS_SEC
-    );
+        // !user && res.status(401).json("Wrong User Name");
 
-    let originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SEC
+        );
 
-    let inputPassword = req.body.password;
+        let originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
 
-    if (originalPassword === inputPassword) {
-      const accessToken = jwt.sign(
-      {
-        id: user._id,
-        isAdmin: user.isAdmin,
-      },
-      process.env.JWT_SEC,
-      { expiresIn: "3d" }
-    );
+        let inputPassword = req.body.password;
 
-    const { password, ...others } = user._doc;
+        if (originalPassword === inputPassword) {
+            const accessToken = jwt.sign({
+                    id: user._id,
+                    isAdmin: user.isAdmin,
+                },
+                process.env.JWT_SEC, { expiresIn: "3d" }
+            );
 
-    res.status(201).json({ ...others, accessToken });
-      
-    } else {
+            const { password, ...others } = user._doc;
 
-      res.status(401).json('wrong password')
-      
+            res.status(201).json({...others, accessToken });
+
+        } else {
+
+            res.status(401).json('wrong password')
+
+        }
+    } catch (error) {
+        res.status(500).json(error);
     }
-  } catch (error) {
-    res.status(500).json(error);
-  }
 });
 
 module.exports = router;
