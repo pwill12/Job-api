94b03d59afce647529830fe2aa7b62c13c9cfe1f
unknown <princesolo919@gmail.com>
2022-07-12 20:43:34 +0100

    stripe added

--- routes/Stripe.js
+++ routes/Stripe.js
@@ -1,20 +1,30 @@
+require("dotenv").config()
 const router = require("express").Router();
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+const stripe = require("stripe")(process.env.STRIPE_KEY)
+const cors = require("cors")
 
-router.post("/payment", (req, res) => {
-    stripe.charges.create({
-            source: req.body.tokenId,
-            amount: 500,
-            currency: "usd",
-        },
-        (stripeErr, stripeRes) => {
-            if (stripeErr) {
-                res.status(500).json(stripeErr);
-            } else {
-                res.status(200).json(stripeRes);
-            }
-        }
-    );
-});
+router.post("/payment", cors(), async(req, res) => {
+    let { amount, id } = req.body
+    try {
+        const payment = await stripe.paymentIntents.create({
+            amount,
+            currency: "USD",
+            description: "Spatula company",
+            payment_method: id,
+            confirm: true
+        })
+        console.log("Payment", payment)
+        res.json({
+            message: "Payment successful",
+            success: true
+        })
+    } catch (error) {
+        console.log("Error", error)
+        res.json({
+            message: "Payment failed",
+            success: false
+        })
+    }
+})
 
-module.exports = router;
+module.exports = router
