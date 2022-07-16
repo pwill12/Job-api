9cec4c59c53d72d64c4b2671e2eda9644e6ee903
unknown <princesolo919@gmail.com>
2022-07-16 21:22:17 +0100

    modified

--- routes/Jobs.js
+++ routes/Jobs.js
@@ -3,69 +3,69 @@ const router = require("express").Router();
 const Jobs = require("../models/Jobs");
 
 const {
-  verifyToken,
-  verifyTokenAndAuthorization,
-  verifyTokenAndAdmin,
+    verifyToken,
+    verifyTokenAndAuthorization,
+    verifyTokenAndAdmin,
+    verifyTokenAndEmployer,
 } = require("./jwtverify");
 
-router.post("/jobs", verifyTokenAndAdmin, async function (req, res) {
-  const newJobsPost = new Jobs(req.body);
-  try {
-    const JobsPost = await newJobsPost.save();
-    res.status(200).json(JobsPost);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
-
-router.get("/findjobs/:id", async function (req, res) {
-  try {
-    const getJobs = await Jobs.findById(req.params.id);
-    res.header("Access-Control-Allow-Origin", "*");
-    res.status(200).json(getJobs);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get("/findjobs", async function (req, res) {
-  let qNew = req.query.new;
-  const qtags = req.query.tags;
-
-  try {
-    //   const getJobs = await Jobs.find()
-    //   res.status(200).json(getJobs);
-
-    if (qNew) {
-      const Jobs = await Jobs.find().sort({ createdAt: -1 }).limit(1);
-      res.header("Access-Control-Allow-Origin", "*");
-      res.status(200).json(Jobs);
-    } else if (qtags) {
-      Jobs.find(
-        {
-          // categories: {
-          //   $in: [qCategory],
-          // },
-          tag: qtags,
-        },
-        (error, data) => {
-          if (error) {
-            console.log(error);
-          } else {
-            // console.log(data);
-            res.header("Access-Control-Allow-Origin", "*");
-            res.status(200).json(data);
-          }
-        }
-      );
-    } else {
-      const getJobs = await Jobs.find();
-      res.header("Access-Control-Allow-Origin", "*");
-      res.status(200).json(getJobs);
+router.post("/jobs", verifyTokenAndAdmin || verifyTokenAndEmployer, async function(req, res) {
+    const newJobsPost = new Jobs(req.body);
+    try {
+        const JobsPost = await newJobsPost.save();
+        res.status(200).json(JobsPost);
+    } catch (error) {
+        res.status(500).json(error);
     }
-  } catch (error) {
-    res.status(500).json(error);
-  }
+});
+
+router.get("/findjobs/:id", async function(req, res) {
+    try {
+        const getJobs = await Jobs.findById(req.params.id);
+        res.header("Access-Control-Allow-Origin", "*");
+        res.status(200).json(getJobs);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get("/findjobs", async function(req, res) {
+    let qNew = req.query.new;
+    const qtags = req.query.tags;
+
+    try {
+        //   const getJobs = await Jobs.find()
+        //   res.status(200).json(getJobs);
+
+        if (qNew) {
+            const Jobs = await Jobs.find().sort({ createdAt: -1 }).limit(1);
+            res.header("Access-Control-Allow-Origin", "*");
+            res.status(200).json(Jobs);
+        } else if (qtags) {
+            Jobs.find({
+                    // categories: {
+                    //   $in: [qCategory],
+                    // },
+                    tag: qtags,
+                },
+                (error, data) => {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        // console.log(data);
+                        res.header("Access-Control-Allow-Origin", "*");
+                        res.status(200).json(data);
+                    }
+                }
+            );
+        } else {
+            const getJobs = await Jobs.find();
+            res.header("Access-Control-Allow-Origin", "*");
+            res.status(200).json(getJobs);
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
 module.exports = router;
