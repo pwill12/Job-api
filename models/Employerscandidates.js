093b5cc6021f9efa36d1dd8e9db3629b1faec203
unknown <princesolo919@gmail.com>
2022-07-24 00:50:37 +0100

    gaggshhs

--- models/Employerscandidates.js
+++ models/Employerscandidates.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 const EmployerscandidatesSchema = new mongoose.Schema({
     employerId: { type: String },
-    jobitems: {
+    jobitems: [{
         user: { type: String },
         jobs: { type: String },
         username: { type: String },
         email: { type: String },
         cover: { type: String },
         projectlinks: { type: String }
-    }
+    }]
 }, { timestamps: true });
 
 
