bde9d0bbefe6dbd9d0187cc0a73be815a48b943a
unknown <princesolo919@gmail.com>
2022-07-24 14:12:55 +0100

    jdhsdhgg

--- routes/employerscandidate.js
+++ routes/employerscandidate.js
@@ -42,7 +42,7 @@ router.get("/candidateapplied", async function(req, res) {
     const qtags = req.query.ids;
     try {
         const users = await Employerscandidate.find({
-            employerId: qtags
+            jobId: qtags
         });
         res.status(200).json(users);
     } catch (error) {
