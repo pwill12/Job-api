4c9a9a04fd24da71ad50653a0f09f14d7d625a60
unknown <princesolo919@gmail.com>
2022-07-24 05:19:22 +0100

    dhshgh

--- models/Employerscandidates.js
+++ models/Employerscandidates.js
@@ -3,14 +3,14 @@ const mongoose = require("mongoose");
 const EmployerscandidatesSchema = new mongoose.Schema({
     jobId: { type: String },
     employerId: { type: String },
-    jobitems: [{
+    jobitems: {
         user: { type: String },
         jobs: { type: String },
         username: { type: String },
         email: { type: String },
         cover: { type: String },
         projectlinks: { type: String },
-    }, ],
+    },
 }, { timestamps: true });
 
 module.exports = mongoose.model(
